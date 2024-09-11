0a79b02fea03184c8ad0c72f0f5f4d03c80ee9b5
Isayas Adhanom <isayasadhanom@gmail.com>
2024-09-11 00:15:35 -0500

    post-class commit

--- src/App.ts
+++ src/App.ts
@@ -8,24 +8,78 @@ import * as gfx from 'gophergfx'
 
 export class App extends gfx.GfxApp
 {
+    private ship: gfx.Mesh2;
+
+    private starfield: gfx.Particles2;
+    private star: gfx.Mesh2;
+
+    private laserSound: HTMLAudioElement;
+
+    private mousePosition: gfx.Vector2;
+
     // --- Create the App class ---
     constructor()
     {
         // initialize the base class gfx.GfxApp
         super();
+
+        this.ship = gfx.Geometry2Factory.createBox();
+        this.star = gfx.Geometry2Factory.createBox();
+
+        this.starfield = new gfx.Particles2(this.star, 200);
+
+        this.laserSound = new Audio('./laser.mp3');
+
+        this.mousePosition = new gfx.Vector2();
     }
 
 
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        this.ship.material.texture = new gfx.Texture('./ship.png');
+        this.star.material.texture = new gfx.Texture('./star.png');
 
+        for(let i =0 ; i < this.starfield.numParticles; i++){
+            this.starfield.particleSizes[i] = Math.random()*0.008+0.002;
+            this.starfield.particlePositions[i].set(Math.random()*2-1,Math.random()*2-1);
+
+        }
+
+        this.starfield.update(true, true);
+
+        this.ship.scale.set(0.08,0.08);
+
+        this.scene.add(this.starfield);
+        this.scene.add(this.ship);
+        
     }
 
+    /**
+     * Method called when the mouse is moved. Subclasses can override this method to handle the event.
+     * 
+     * @param event - The MouseEvent object associated with the mouse movement
+     */
+    onMouseDown(event: MouseEvent): void {
+        this.laserSound.play();
+        this.laserSound.currentTime = 0;
+    }
     
+    /**
+     * Method called when the mouse is moved. Subclasses can override this method to handle the event.
+     * 
+     * @param event - The MouseEvent object associated with the mouse movement
+     */
+    onMouseMove(event: MouseEvent): void {
+        
+        this.mousePosition.copy(this.getNormalizedDeviceCoordinates(event.x, event.y));
+    }
+
+
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
+        this.ship.lookAt(this.mousePosition);
 
     }
 }
